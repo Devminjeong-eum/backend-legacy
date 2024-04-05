29e0edbad07550795d52d8c08816d3db2752ff64
tls3254 <bikim9923@gmail.com>
2024-04-06 01:31:37 +0900

    init

---
+++ src/main/java/com/example/devminjeongeum/DevminjeongEumApplication.java
@@ -0,0 +1,13 @@
+package com.example.devminjeongeum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevminjeongEumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevminjeongEumApplication.class, args);
+    }
+
+}
