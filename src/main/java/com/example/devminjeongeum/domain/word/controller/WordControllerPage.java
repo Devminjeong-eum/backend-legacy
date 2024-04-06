29713f90b54faca08583ab874dd948cb2e3651ed
tls3254 <bikim9923@gmail.com>
2024-04-06 22:51:32 +0900

    feat: 단어 전체 페이지 완성

--- src/main/java/com/example/devminjeongeum/domain/word/controller/WordControllerPage.java
+++ src/main/java/com/example/devminjeongeum/domain/word/controller/WordControllerPage.java
@@ -8,27 +8,44 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class WordControllerPage {
 
     private final WordService wordService;
 
     @GetMapping("/words")
-    public String wordAll(Model model,
-                          @RequestParam(defaultValue = "0") int page,
-                          @RequestParam(defaultValue = "10") int size){
+    public ResponseEntity<Map<String, Object>> wordAll(Model model,
+                                                       @RequestParam(defaultValue = "0") int page,
+                                                       @RequestParam(defaultValue = "10") int size){
         Page<Word> wordPage = wordService.getAllword(page -1,size);
-        model.addAttribute("wordPage", wordPage);
-        return "Allword";
+        List<Word> wordList = wordPage.getContent();
+
+        long totalItems = wordPage.getTotalElements();
+        int offset = wordPage.getNumber();
+        int pageSize = wordPage.getSize();
+        int totalPages = wordPage.getTotalPages();
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("data", wordList);
+        response.put("total", totalItems);
+        response.put("offset", offset);
+        response.put("limit", pageSize);
+        response.put("totalPages", totalPages);
+
+        return ResponseEntity.ok().body(response);
     }
 
     @GetMapping("/word/{wordId}")
