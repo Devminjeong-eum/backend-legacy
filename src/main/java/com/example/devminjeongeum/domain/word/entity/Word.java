d287d6a5e538b7fb509ea36549d6d81719770812
tls3254 <bikim9923@gmail.com>
2024-04-07 22:21:05 +0900

    fix : 단어 발음 추가

--- src/main/java/com/example/devminjeongeum/domain/word/entity/Word.java
+++ src/main/java/com/example/devminjeongeum/domain/word/entity/Word.java
@@ -12,7 +12,7 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Word extends Auditable {
+public class Word {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long wordId;
